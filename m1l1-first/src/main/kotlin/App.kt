4b2667ea2e7a037057baabd8dc25d509efe94b0a
Сергей Окатов <sokatov@gmail.com>
2026-09-15 10:16:46 +0500

    m1l1-first merge

--- m1l1-first/src/main/kotlin/App.kt
+++
@@ -1,9 +0,0 @@
-package ru.otus.otuskotlin.app
-
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    val message = "Hello, " + name + "!"
-    println(message)
-}
